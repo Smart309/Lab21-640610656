a0d88e835f9b5837048b4d5f76c9198590809514
Smart309 <pattharapon_takham@cmu.ac.th>
2022-02-01 19:04:45 +0700

    "UP ALL R1"

--- lab21_3.cpp
+++ lab21_3.cpp
@@ -8,9 +8,10 @@
 using namespace std;
 
 struct student{
-
-    //Define struct student with four member (name ,id , gender, gpa);
-    
+	string name;
+	int id;
+	char gender;
+	float gpa;  
 };
 
 struct course{
@@ -20,7 +21,7 @@ struct course{
 	vector<student *> student_list;
 };
 
-student * findstudent(vector<student> allstudents,int key){ //There is something wrong in this line.
+student * findstudent(vector<student> &allstudents,int key){ 
 	for(unsigned int i = 0; i < allstudents.size(); i++){
 		if(allstudents[i].id  == key) return &allstudents[i];
 	}
@@ -62,10 +63,16 @@ int main(){
 	string textline;
 	
 	while(getline(student_file,textline)){
-		student s; 
-		
-		//Use sscanf() to split the values in textline and assign those values to the members of struct s;
-
+		student s;
+		char name[100];
+		int id;
+		char gender[1];
+		float gpa;
+		sscanf(textline.c_str(),"%[^,],%i,%c,%f",name,&id,gender,&gpa);
+		s.name = name;
+		s.id = id;
+		s.gender = *gender;
+		s.gpa = gpa;
 		allstudents.push_back(s); 		
 	}
 	
@@ -83,18 +90,28 @@ int main(){
 			if(textline == "> Students"){
 				state = 3;
 			}else{
-			
-			    //Append (push_back) textline to lecture_list[] of the recently added course in allcourses[];
-			    
+				if (allcourses.size() == 1)
+				{
+					allcourses[0].lecture_list.push_back(textline);
+				}else if(allcourses.size() == 2 ){
+					allcourses[1].lecture_list.push_back(textline);
+				}else{
+					allcourses[2].lecture_list.push_back(textline);
+				}
 			}			
 		}else{
 			if(textline == "---------------------------------------"){
 				state = 1;
 			}else{
 				student *p = findstudent(allstudents,atof(textline.c_str()));
-				
-				//Append (push_back) p to student_list of the recently added course in allcourses[];
-				
+				if (allcourses.size() == 1)
+				{
+					allcourses[0].student_list.push_back(p);
+				}else if(allcourses.size() == 2 ){
+					allcourses[1].student_list.push_back(p);
+				}else{
+					allcourses[2].student_list.push_back(p);
+				}
 			}
 		}
 	}
